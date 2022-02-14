d5e2b54b1e2162e20c4726707275dd8fb753e4b7
André Kuratomi <andrekuratomi@gmail.com>
2022-02-14 10:54:11 -0300

    feat: error middleware implemented on app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import router from "./routes";
+import { handleError } from "./middlewares/errors.middlewares";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(router);
+
+app.use(handleError);
 
 export default app;
