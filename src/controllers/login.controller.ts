a4320923060817fc7edd5fa384c96a88b6ed79b5
Pierre Kalil <pierrekalil1@gmail.com>
2022-02-14 17:24:11 -0300

    feat: create controller login

---
+++ src/controllers/login.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { LoginUserService } from "../services/login.service";
+import jwt from "jsonwebtoken";
+
+export class LoginController {
+  async handle(req: Request, res: Response) {
+    const login = new LoginUserService();
+    const { email, password } = req.body;
+
+    const token = await login.execute(email, password);
+    return res.status(200).send({ token });
+  }
+}
