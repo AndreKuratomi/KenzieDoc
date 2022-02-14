82c133c90adc9ca0c14852918190c743d919fd6e
David <david.avanci@gmail.com>
2022-02-14 16:25:54 -0300

    add professional controllers

--- src/routes/professional.routes.ts
+++ src/routes/professional.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
+import {
+  CreateProfessionalController,
+  DeleteProfessionalController,
+  ProfessionalsListController,
+  UpdateProfessionalController,
+} from "../controllers/professional.controller";
+
+const createProfessionalController = new CreateProfessionalController();
+const updateProfessionalController = new UpdateProfessionalController();
+const deleteProfessionalionalController = new DeleteProfessionalController();
+const professionalListController = new ProfessionalsListController();
 
 const professionalsRouter = Router();
 
-professionalsRouter.post("");
-professionalsRouter.get("");
-professionalsRouter.patch("/:id");
-professionalsRouter.delete("/:id");
+professionalsRouter.post("", createProfessionalController.handle);
+professionalsRouter.get("", professionalListController.handle);
+professionalsRouter.patch("/:id", updateProfessionalController.handle);
+professionalsRouter.delete("/:id", deleteProfessionalionalController.handle);
 
 export default professionalsRouter;
