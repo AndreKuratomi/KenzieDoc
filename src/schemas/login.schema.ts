11b1cf87ddc8fe9db102ca61b8321a7c549691d6
André Kuratomi <andrekuratomi@gmail.com>
2022-02-17 15:25:49 -0300

    feat: Login schema file drafted

---
+++ src/schemas/login.schema.ts
@@ -0,0 +1,6 @@
+import * as yup from "yup";
+
+export const LoginSchema = yup.object().shape({
+  email: yup.string().email().required("'email' is a required field!"),
+  password: yup.string().required("'password' is a required field!").min(4),
+});
