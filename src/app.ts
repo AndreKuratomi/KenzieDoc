45c892535c3e7cd7a5253c7e16326b15456758f8
David <david.avanci@gmail.com>
2022-02-21 14:00:45 -0300

    fix swagger

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,14 @@ import express, { Request, Response } from "express";
 import router from "./routes";
 import { handleError } from "./middlewares/errors.middlewares";
 import swaggerUiExpress from "swagger-ui-express";
-import swaggerDocument from "./swagger.js";
+import swaggerDocument from "./swagger.json";
 import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors({ origin: "*", allowedHeaders: "Authorization" }));
+app.use(cors());
 
 app.use(
   "/docs",
