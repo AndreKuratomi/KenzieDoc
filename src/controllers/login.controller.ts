ce7fb293100161ff013ecfd9134c679dd989143e
David <david.avanci@gmail.com>
2022-02-22 09:28:58 -0300

    bug fix

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -10,7 +10,7 @@ export class LoginController {
       const token = await login.execute(email, password);
       return res.status(200).send({ token });
     } catch (error: any) {
-      return res.status(error.statusCode).json({ message: error.message });
+      return res.status(400).json({ message: error.message });
     }
   }
 }
